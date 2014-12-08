739cde20be6b8d78eec3cb4a8eba619b28ee9f2e
Трубников Александр <trubnikov@corp.sputnik.ru>
2014-12-08 13:14:02 +0300

    init django example

---
+++ examples/dj/dj/urls.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns(
+    '',
+    # url(r'^$', 'dj.views.home', name='home'),
+    # url(r'^dj/', include('dj.foo.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
